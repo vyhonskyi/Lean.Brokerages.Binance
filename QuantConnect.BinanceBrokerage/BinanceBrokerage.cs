be736b88c46981fb4df377d50c2960989b857908
vitaliy.v <vitaliy.v@ualeaders.com>
2023-07-19 18:04:23 +0300

    Subscription validation removed

--- QuantConnect.BinanceBrokerage/BinanceBrokerage.cs
+++ QuantConnect.BinanceBrokerage/BinanceBrokerage.cs
@@ -564,7 +564,7 @@ namespace QuantConnect.BinanceBrokerage
                 Connect();
             };
 
-            ValidateSubscription();
+            //ValidateSubscription();
         }
 
         /// <summary>
